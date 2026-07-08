719aa8baefdeb96ae2fb2ac5f139009f3c25ec44
Mirza Alhaj Baig <alhajbaig2006@gmail.com>
2026-07-08 18:10:43 +0530

    Time: 11 ms (22.74%), Space: 12.4 MB (81.64%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        i = len(num1) - 1
+        j = len(num2) - 1
+        carry = 0
+        ans = ""
+
+        while i >= 0 or j >= 0 or carry:
+
+            n1 = ord(num1[i]) - ord('0') if i >= 0 else 0
+            n2 = ord(num2[j]) - ord('0') if j >= 0 else 0
+
+            total = n1 + n2 + carry
+
+            ans = str(total % 10) + ans
+            carry = total // 10
+
+            i -= 1
+            j -= 1
+
+        return ans
